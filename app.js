9bedfc9419dc58165503d3e32a1aac7c94ccd095
csyuwang <csyuwang@sina.com>
2016-04-12 01:04:21 +0800

    修改app.js中的端口

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var http = require('http');
 var path = require('path');
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 app.use("/", express.static(path.join(__dirname, 'build')));
 
 // 创建服务端
-http.createServer(app).listen('9000', function() {
+http.createServer(app).listen(port, function() {
 	console.log('Server Started');
 });
